61b358ca1c79ef91ceb303986eb63f4d1cdf6394
adityaj56 <adityajangid2015@gmail.com>
2023-02-13 22:25:49 +0530

    added ejs as view engine

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+//import express module to the main entry point file of our project
 const express = require('express');
+//store express function in app constant
 const app = express();
 const port = 8000;
 
+//This will create a middleware which route our request from the entry page to further pages
 app.use('/', require('./routes/index'));
 
+//For listening the request from designated port
 app.listen(port, function(err){
     if(err){
         console.log(`Error while connecting to the server: ${err}`);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
+//This file is entry point for all the routes we will define many middlewares for further routing the requests
 const express = require('express');
+
+//this contains all the router functions
 const router = express.Router();
+
+//As this index.js will handle all the rounting for home directly so we import homeController in here
 const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.home);
